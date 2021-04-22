371ca21ab1361be80f5ea583ea0a1397177975d3
DeXmas <idexmas@gmail.com>
2021-04-23 02:03:09 +0300

    Improve receiver prototype

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -8,10 +8,24 @@
 
 #include <Arduino.h>
 
+#define SENSOR_COUNT 32
+#define LINE_HEIGHT 8
+
+struct SSensorInfo
+{
+    int uid;
+    float temp;
+    float hum;
+    float batt;
+    float time;
+};
+
 uint8_t DSdata[16];
 uint8_t rf_setup;
-int recieved = 0;
-int wait = 0;
+char str_buf[32];
+int render_delay = 0;
+int status = 0;
+SSensorInfo sensors[SENSOR_COUNT];
 
 U8GLIB_ST7920_128X64 u8g(4, 3, 2, U8G_PIN_NONE);
 
@@ -20,10 +34,16 @@ void setup() {
     delay(2);
     Serial.println("Mirf Init");
 
-    //Need for delay 50ms before init dispaly
-    delay(50);
+    for(int i=0; i<SENSOR_COUNT; i++)
+    {
+        sensors[i].uid = -1;
+        sensors[i].temp = 0.0f;
+        sensors[i].hum = 0.0f;
+        sensors[i].batt = 0.0f;
+        sensors[i].time = 0.0f;
+    }
 
-    u8g.setFont(u8g_font_8x13); 
+    u8g.setFont(u8g_font_6x10); 
     u8g.setColorIndex(1);
     
     mirf_init();
@@ -44,58 +64,116 @@ void setup() {
     mirf_config_register(EN_RXADDR, 0x7F);
 
     Serial.println("Setup OK");
-    u8g.drawStr(0, 0, "Wait...");
+    
+    u8g.firstPage();
+    do { 
+        u8g.setPrintPos(42, 32);
+        u8g.print("No data...");
+    } while( u8g.nextPage() );  
+
+    status = 0;
+}
+
+SSensorInfo& getSensor(int _uid)
+{
+    for(int i=0; i<SENSOR_COUNT; i++)
+    {
+        if(sensors[i].uid == _uid)
+            return sensors[i];
+    }
+    for(int i=0; i<SENSOR_COUNT; i++)
+    {
+        if(sensors[i].uid == -1)
+        {
+            sensors[i].uid = _uid;
+            return sensors[i];
+        }
+    }
+    float time = 0.0f;
+    int id = 0;
+    for(int i=0; i<SENSOR_COUNT; i++)
+    {
+        if(sensors[i].time > time)
+            id = i;
+    }
+
+    sensors[id].uid = _uid;
+    return sensors[id];
 }
 
 void loop() {
-    //wdt_reset();
-    delay(500);
-    wait++;
 
-    char str_buf[32];
+    delay(100);
+    render_delay += 100;
+    
+    for(int i=0; i<SENSOR_COUNT; i++)
+    {
+        if(sensors[i].uid != -1)
+            sensors[i].time += 0.1f;
+    }
 
     if(mirf_data_ready())  {
         mirf_read_register( STATUS, &rf_setup, sizeof(rf_setup));
         mirf_get_data(DSdata);
 
-        recieved++;
-        wait=0;
+        if(status == 0)
+        {
+            status = 1;
+        }
+
+        SSensorInfo& sensor = getSensor(DSdata[6]);
+
+        sensor.time = 0.0f;
+        sensor.batt = float(DSdata[5]) / 255.0f;
 
         int iH = (DSdata[0] << 8) | DSdata[1];
-        float fH = iH / 10.0f;
+        sensor.hum = iH / 10.0f;
 
         int iT = ((DSdata[2] & 0x7f ) << 8 ) + DSdata[3];
         if (DSdata[2] & 0x80)
             iT *= -1;
             
-        float fT = iT / 10.0f;
-
-        sprintf(str_buf, "#%02d t%02d' h%02d%% (%02dv)", DSdata[6], (int)(fT > 0 ? fT : -fT), (int)fH, DSdata[5]);
+        sensor.temp = iT / 10.0f;
 
         Serial.print("ID: ");
-        Serial.print(DSdata[6]);
+        Serial.print(sensor.uid);
         Serial.print(" Vbat.: ");
-        Serial.print(DSdata[5]);
+        Serial.print(sensor.batt);
         Serial.print(" Humid.: ");
-        Serial.print(fH);
+        Serial.print(sensor.hum);
         Serial.print("%, Temp.: ");
-        Serial.print(fT);
+        Serial.print(sensor.temp);
         Serial.print("' (CRC: ");
         Serial.print(DSdata[4], HEX);
         Serial.println(")");
     }
 
-    u8g.firstPage();
-    do { 
-        u8g.setPrintPos(0, 10);
-        u8g.print(str_buf);
+    if(status > 0 && render_delay >= 500)
+    {
+        render_delay = 0;
 
-        //sprintf(str_buf,"recv: %03d wait: %03d", recieved%1000, (wait/2)%1000);
-        //int pcnt = (DSdata[5] * 99) / 255;
-        //sprintf(str_buf,"I:%01d B:%02d T:%03d C:%03d", DSdata[6], (uint8_t)pcnt, (wait/2)%1000, recieved%1000);
-        //sprintf(str_buf,"5:%03d 6:%03d  ", DSdata[5], DSdata[6]);
+        u8g.firstPage();
+        do { 
+            int y_pos = 7;
+            for(int i=0; i<SENSOR_COUNT; i++)
+            {
+                if(sensors[i].uid != -1)
+                {
+                    sprintf(str_buf, "#%02d t%02.2f' h%02.2f%% (%.2f%%)", sensors[i].uid, sensors[i].temp, sensors[i].hum, sensors[i].batt);
+                    u8g.setPrintPos(0, y_pos);
+                    u8g.print("#");
+                    u8g.print(sensors[i].uid, 1);
+                    u8g.print(" t");
+                    u8g.print(sensors[i].temp, 1);
+                    u8g.print("' h");
+                    u8g.print(sensors[i].hum, 1);
+                    u8g.print("% b");
+                    u8g.print(int(sensors[i].batt * 100));
+                    u8g.print("%");
 
-        //u8g.setPrintPos(0, 30);
-        //u8g.print(str_buf);
-     } while( u8g.nextPage() );
+                    y_pos += LINE_HEIGHT;
+                }
+            }
+        } while( u8g.nextPage() );
+    }
 }
